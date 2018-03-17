261d926e63d780fc79cb6aeeb45bb79006a86eba
bilaizi <2512075115@qq.com>
2018-03-17 23:10:08 +0800

    Delete IssueRepository.java

--- app/src/main/java/com/android/examples/githubissues/repositories/IssueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.android.examples.githubissues.repositories;
-
-import android.arch.lifecycle.LiveData;
-
-import com.android.examples.githubissues.entities.ApiResponse;
-
-public interface IssueRepository {
-    LiveData<ApiResponse> getIssues(String owner, String repo);
-}
