b3521f8f22eb284fc168a6908d112d1e7645df39
bilaizi <2512075115@qq.com>
2018-03-18 15:03:05 +0800

    Update controlstructure.cpp

--- controlstructure.cpp
+++ controlstructure.cpp
@@ -1,8 +1,32 @@
 #include <chrono>
 #include <iostream>
 using namespace std;
+
 enum class MessageSeverity{                               
     information,
     warning,
     fatal,
 };
+
+auto start = std::chrono::steady_clock::now(); 
+
+void writeElapsedTime(){                                   
+    auto now = std::chrono::steady_clock::now();  
+    std::chrono::duration<double> diff = now - start;
+    std::cerr << diff.count() << " sec. elapsed: ";
+}
+
+void writeInformation(){ std::cerr << "information" << std::endl; }
+void writeWarning(){ std::cerr << "warning" << std::endl; }
+void writeUnexpected(){ std::cerr << "unexpected" << std::endl; }
+
+void writeMessage(MessageSeverity messageseverity){
+    writeElapsedTime(); 
+    if (MessageSeverity::information == messageseverity)
+	    writeInformation();
+    else if (MessageSeverity::warning == messageseverity)
+	    writeWarning();
+    else{
+	    writeUnexpected();
+    }
+}
